7e0343f8ebd0bd186bca01c1207018a269c262a3
taku <taku@example.com>
2023-04-29 13:13:11 +0000

    PHP/Laravel 09 Routingについて理解する 課題3,4 修正1

--- app/Http/Controllers/AAAController.php
+++ app/Http/Controllers/AAAController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class AAAController extends Controller
 {
     public function bbb() {
-        return view('admin.test.create');
+        return view('XXX.create');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,30 +13,55 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// ControllerやActionを指定せずに直接Viewファイルを表示する
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 use App\Http\Controllers\Admin\NewsController;
 
 // prefix:Route設定するとき引数で指定したURLの配下に反映させられる
 // admin配下のURLにfunctionの処理が反映される
-Route::controller(NewsConroller::class)->prefix('admin')->group(function(){
-    // /create にアクセスされるとNewsConrollerのaddメソッドの処理を実行する
+Route::controller(NewsController::class)->prefix('admin')->group(function(){
+    // /create にアクセスされるとNewsControllerのaddメソッドの処理を実行する
     Route::get('news/create', 'add');
 });
 
+// シンプルに書いたパターン
+// Route::get('/admin/news/create',[NewsController::class,'add2']);
+
+
 // PHP/Laravel 09 Routingについて理解する 課題3
-Route::controller(AAAController::class)->prefix('admin')->group(function(){
- Route::get('test/create', 'bbb');
+use App\Http\Controllers\AAAController;
+Route::controller(AAAController::class)->group(function(){
+ Route::get('XXX', 'bbb');
 });
 
+// 課題3 シンプルに書いたパターン
+// Route::get('/XXX',[AAAController::class,'bbb']);
+
+
 // PHP/Laravel 09 Routingについて理解する 課題4
+use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->group(function(){
     Route::get('profile/create', 'add');
-});
-
-// PHP/Laravel 09 Routingについて理解する 課題4
-Route::controller(ProfileController::class)->prefix('admin')->group(function(){
     Route::get('profile/edit', 'edit');
 });
+
+// 課題4 シンプルに書いたパターン
+// Route::get('admin/profile/create',[ProfileController::class,'add']);
+// Route::get('admin/profile/edit',[ProfileController::class,'edit']);
+
+
+// 入れ子で書いたパターン
+// Route::prefix('admin')->group(function (){
+//     Route::controller(NewsController::class)->prefix('news')->group(function (){
+//         // /admin/news/create -> add()    
+//     });
+//     Route::controller(ProfileController::class)->prefix('profile')->group(function (){
+//         // /admin/profile/edit -> edit()
+//         Route::get('edit', 'edit');
+//     });
+    
+// });
