f81b265c5ecf5750d42b129cb00264ad4c0556e8
taku <taku@example.com>
2023-04-29 03:35:29 +0000

    PHP/Laravel 09 Routingについて理解する 課題3,4

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Admin\NewsController;
+
+// prefix:Route設定するとき引数で指定したURLの配下に反映させられる
+// admin配下のURLにfunctionの処理が反映される
+Route::controller(NewsConroller::class)->prefix('admin')->group(function(){
+    // /create にアクセスされるとNewsConrollerのaddメソッドの処理を実行する
+    Route::get('news/create', 'add');
+});
+
+// PHP/Laravel 09 Routingについて理解する 課題3
+Route::controller(AAAController::class)->prefix('admin')->group(function(){
+ Route::get('test/create', 'bbb');
+});
+
+// PHP/Laravel 09 Routingについて理解する 課題4
+Route::controller(ProfileController::class)->prefix('admin')->group(function(){
+    Route::get('profile/create', 'add');
+});
+
+// PHP/Laravel 09 Routingについて理解する 課題4
+Route::controller(ProfileController::class)->prefix('admin')->group(function(){
+    Route::get('profile/edit', 'edit');
+});
