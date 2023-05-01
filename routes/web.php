bd2d0d7a844fee343157fb7985d76c69dfedc209
taku <taku@example.com>
2023-05-01 02:39:17 +0000

    PHP/Laravel 11 Bladeテンプレートの継承とLaravel Mixの使い方を活用してみよう 課題3~8

--- routes/web.php
+++ routes/web.php
@@ -65,3 +65,7 @@ Route::controller(ProfileController::class)->prefix('admin')->group(function(){
 //     });
     
 // });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
