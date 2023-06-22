70e9f1cd3522f0354ad12b5f3884dd8301f97dbe
taku <taku@example.com>
2023-06-22 13:49:54 +0000

    PHP/Laravel 19 一般ユーザーが読むニュースサイトを作成しよう テキスト

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\News;
+
+class NewsController extends Controller
+{
+    //
+    public function index(Request $request) {
+        
+        // 全ての記事を投稿日時順に新しいほうから並べ替える
+        $posts = News::all()->sortByDesc('updated_at');
+        
+        // 記事が1以上あったら
+        if (count($posts) > 0) {
+            
+            // shift()メソッドは、配列の最初のデータを削除し、その値を返す。配列を左にシフトする
+            // $headline に最新の記事を代入。$postsに最新以外の記事が格納される。最新の記事とそれ以外の記事の表記を変えたい。
+            $headline = $posts->shift();
+            
+        } else {
+            $headline = null;
+        }
+        
+        // news/index.blade.php ファイルに渡している
+        // また View テンプレートに headline、posts、という変数を渡している
+        return view('news.index', ['headline' => $headline, 'posts' => $posts]);
+    }
+}
