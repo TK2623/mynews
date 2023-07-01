b77e9701be71786ba177c6cde5b9a5c2a623b3e1
taku <taku@example.com>
2023-07-01 23:05:32 +0000

    プロフィール画像 　はてな完成 コメント削除

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,15 +14,6 @@ class ProfileController extends Controller
         // 全てのプロフィール情報を取得する
         $posts = Profile::all();
         
-        // $profile = new Profile;
-        // foreach($posts as $post) {
-        //     if ($post->image_path == null) {
-        //         dd($post);
-        //         // $question = $post->create(['profileimage'=> $path]);
-        //         dd($post);
-        //     }
-        // }
-        
         return view('profile.index', ['posts' => $posts]);
     }
     
